5979bbb6328a4906050de293903a7010882436ce
Minhly <minh.y.ly@hotmail.com>
2021-01-28 19:49:08 +0100

    Main

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+class PasswordGenerator {
+    // Mine arrays til tal, specialtegn, store og små bogstaver
+	private $talArray = array( "1", "2","3", "4", "5", "6", "7", "8", "9", "0" );
+	private $uppercaseArray = array( "A", "B", "C", "D", "E", "F", "G", "H",
